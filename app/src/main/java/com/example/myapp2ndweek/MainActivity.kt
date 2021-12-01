eb2e6310258dd43bc61210d1a4e23f4854383ae7
GFox <Foxxdim@mail.ru>
2021-12-01 17:18:52 +0300

    Work about Top Bar

--- app/src/main/java/com/example/myapp2ndweek/MainActivity.kt
+++ app/src/main/java/com/example/myapp2ndweek/MainActivity.kt
@@ -2,32 +2,71 @@ package com.example.myapp2ndweek
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import androidx.databinding.DataBindingUtil
 import com.example.myapp2ndweek.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
-    private var binding: ActivityMainBinding? = null
+
+class MainActivity : AppCompatActivity(),BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private var binding:ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        setSupportActionBar(binding?.topAppBar)
+
+
         supportFragmentManager.beginTransaction().replace(R.id.content, Home()).commit()
 
         binding?.bottomNav?.setOnNavigationItemSelectedListener(this)
         binding?.bottomNav?.selectedItemId = R.id.homeItemBottomNav
+
+        binding?.topAppBar?.setOnMenuItemClickListener { menuItem:MenuItem ->
+
+            when(menuItem.itemId) {
+
+                R.id.favoritesItemTopNav -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.content, Favorites()).commit()
+
+                    true
+
+                }
+
+                R.id.settingsItemTopNav -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.content, Settings()).commit()
+
+                    true
+
+                }
+
+                else -> false
+            }
+
+        }
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
         when(item.itemId) {
             R.id.homeItemBottomNav -> supportFragmentManager.beginTransaction().replace(R.id.content, Home()).commit()
             R.id.shopItemBottomNav -> supportFragmentManager.beginTransaction().replace(R.id.content, Shop()).commit()
             R.id.deliveryItemBottomNav -> supportFragmentManager.beginTransaction().replace(R.id.content, Delivery()).commit()
             R.id.accountItemBottomNav -> supportFragmentManager.beginTransaction().replace(R.id.content, Account()).commit()
         }
+
+        return true
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.top_menu, menu)
 
         return true
     }
